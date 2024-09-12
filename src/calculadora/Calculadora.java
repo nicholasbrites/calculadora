22c2df227288c2674f7b71ad5c58e963e3853f96
Nicholas Brites <nickbrites1@gmail.com>
2024-09-12 00:50:34 -0300

    calculadora

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+import apresentacao.frmCalculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        
+        frmCalculadora frmC = new frmCalculadora(null, true);
+        frmC.setVisible(true);
+        
+    }
+    
+}
